03467e1d2f78bb2a01c2d45923a30b04d2505d1b
Stathis Kolovos <stathiskolovos@MacBook.local>
2025-01-30 18:01:00 +0200

    Task 3: CartItems component

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -4,11 +4,12 @@ export const CartSlice = createSlice({
   name: 'cart',
   initialState: {
     items: [], // Initialize items as an empty array
+    totalNumOfItems: 0 // variable dedicated to counting the total number of items added to the cart
   },
   reducers: {
     addItem: (state, action) => {
       // in shopping cart i need image, name, cost => payload
-      const { image, name, cost} = action.payload;
+      const { image, name, cost, quantity} = action.payload;
       const inCart = state.items.find(item => item.name === name); // check if plant already in cart
       if (inCart) { // if in cart +1 quantity
         inCart.quantity++;
@@ -16,15 +17,22 @@ export const CartSlice = createSlice({
         state.items.push({image, name, cost, quantity: 1});
       }
 
+      state.totalNumOfItems += 1; // incr by 1
+
     },
     removeItem: (state, action) => {
-      state.items = state.items.filter(item => item.name !== action,payload); // filter out plant
+      const {image, name, cost, quantity} = action.payload;
+      state.items = state.items.filter(item => item.name !== name); // filter out plant
+
+      state.totalNumOfItems -= quantity; // sub the amount of plants removed from cart
     },
     updateQuantity: (state, action) => {
       const {image, name, cost, quantity} = action.payload;
-      const updateItem = state.items.find(item => items.name === name);
+      const updateItem = state.items.find(item => item.name === name);
       if (updateItem) {
+        const diff = quantity - updateItem.quantity;
         updateItem.quantity = quantity;
+        state.totalNumOfItems += diff;
       }
     },
   },
